f50277007d29a6ccc57fe81dff22f311fd257045
pokesoftech <fivewisdomzero@gmail.com>
2017-12-15 15:09:12 -0500

    implement logic for model and controller

--- app/controllers/g_analytics_controller.rb
+++ app/controllers/g_analytics_controller.rb
@@ -1,13 +1,19 @@
 class GAnalyticsController < ApplicationController
     # GET /g_analytics
     def index
+
     end
 
     # GET /g_analytics/:start_date/:end_date
     def get_by_date_range
+        start_date = params[:start_date]
+        end_date = params[:end_date]
+        json_response(GAnalytic.get_by_date_range(start_date, end_date))
     end
 
     # GET /g_analytics/:start_date
     def get_by_now
+        start_date = params[:start_date]
+        json_response(GAnalytic.get_by_now(start_date))
     end
 end
--- app/models/g_analytic.rb
+++ app/models/g_analytic.rb
@@ -1,3 +1,6 @@
 class GAnalytic < ApplicationRecord
     self.table_name = "ga_test"
+
+    scope :get_by_date_range, ->(start_date, end_date) { where("date > ? && date < ?", start_date, end_date) }
+    scope :get_by_now, ->(start_date) { where("date > ? && date < ?", start_date, Time.now.strftime('%Y%m%d')) }
 end
