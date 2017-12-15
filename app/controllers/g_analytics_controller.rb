412003a2fa9561587c0c2831d6b2705739181bc9
pokesoftech <fivewisdomzero@gmail.com>
2017-12-15 17:00:59 -0500

    modify api

--- app/controllers/g_analytics_controller.rb
+++ app/controllers/g_analytics_controller.rb
@@ -8,12 +8,14 @@ class GAnalyticsController < ApplicationController
     def get_by_date_range
         start_date = params[:start_date]
         end_date = params[:end_date]
-        json_response(GAnalytic.get_by_date_range(start_date, end_date))
+        @analytics = GAnalytic.get_by_date_range(start_date, end_date)
+        render json: @analytics
     end
 
     # GET /g_analytics/:start_date
     def get_by_now
         start_date = params[:start_date]
-        json_response(GAnalytic.get_by_now(start_date))
+        @analytics = GAnalytic.get_by_now(start_date)
+        render json: @analytics
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/g_analytics/:start_date/:end_date', to: 'g_analytics#get_by_date_range'
+  get '/g_analytics/:start_date', to: 'g_analytics#get_by_now'
 end
