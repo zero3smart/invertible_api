96abb4ebd349a264c1bfae6e46b1d36c5389d4f2
pokesoftech <fivewisdomzero@gmail.com>
2017-12-16 20:59:34 -0500

    chagne sock file name

--- config/puma.rb
+++ config/puma.rb
@@ -30,7 +30,7 @@ rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind 'unix://#{shared_dir}/sockets/puma.sock'
+bind 'unix://#{shared_dir}/sockets/invertible_api-puma.sock'
 
 # Logging
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
