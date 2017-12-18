534aef5983a9b466b96dee71323cd350ae284f8e
pokesoftech <fivewisdomzero@gmail.com>
2017-12-18 01:57:23 -0500

    add database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,8 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 set :rvm_ruby_version, '2.4.2@invertible_api'
 
+set :linked_files, %w{config/database.yml}
+
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
