1431aa483d7543fb53646f5d039a7048d78b5cae
pokesoftech <fivewisdomzero@gmail.com>
2017-12-20 04:50:17 -0500

    remove unused code

--- config/application.rb
+++ config/application.rb
@@ -30,16 +30,6 @@ module InvertibleApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    # Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins '*'
-
-    #     resource '*',
-    #       headers: :any,
-    #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    #   end
-    # end
-
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
