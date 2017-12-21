c27a5dcf92d9f9b8aa317c4da3b13af19f60d2b8
billionsoftech <fivewisdomzero@gmail.com>
2017-12-21 12:34:39 -0500

    add a api for sum

--- app/controllers/g_analytics_controller.rb
+++ app/controllers/g_analytics_controller.rb
@@ -18,4 +18,12 @@ class GAnalyticsController < ApplicationController
         @analytics = GAnalytic.get_by_now(start_date)
         render json: @analytics
     end
+
+    # GET /g_analytics_1/:start_date/:end_date
+    def get_analytics
+        start_date = params[:start_date]
+        end_date = params[:end_date]
+        @analytics = GAnalytic.get_by_date_range(start_date, end_date).select("sum(sessions)")
+        render json: @analytics
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/g_analytics/:start_date/:end_date', to: 'g_analytics#get_by_date_range'
   get '/g_analytics/:start_date', to: 'g_analytics#get_by_now'
+  get '/g_analytics_1/:start_date/:end_date', to: 'g_analytics#get_analytics'
 end
