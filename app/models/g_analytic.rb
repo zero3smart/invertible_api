7e3119e88b2c1296cab9a0b1273a21bd130548ee
pokesoftech <fivewisdomzero@gmail.com>
2017-12-20 20:18:42 -0500

    change filter for date

--- app/models/g_analytic.rb
+++ app/models/g_analytic.rb
@@ -1,7 +1,7 @@
 class GAnalytic < ApplicationRecord
     self.table_name = "ga_test"
 
-    scope :get_by_date_range, ->(start_date, end_date) { where("date > ? and date < ?", start_date, end_date) }
-    scope :get_by_now, ->(start_date) { where("date > ? and date < ?", start_date, Time.now.strftime('%Y%m%d')) }
+    scope :get_by_date_range, ->(start_date, end_date) { where("date >= ? and date <= ?", start_date, end_date) }
+    scope :get_by_now, ->(start_date) { where("date >= ? and date <= ?", start_date, Time.now.strftime('%Y%m%d')) }
 
 end
