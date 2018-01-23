f1964daa5bfe9c75f57d32afc33ac86f4216f550
billionsoftech <fivewisdomzero@gmail.com>
2018-01-23 12:07:04 -0500

    change table name

--- app/models/g_analytic.rb
+++ app/models/g_analytic.rb
@@ -1,5 +1,5 @@
 class GAnalytic < ApplicationRecord
-    self.table_name = "ga_test"
+    self.table_name = "explorer"
 
     scope :get_by_date_range, ->(start_date, end_date) { where("date >= ? and date <= ?", start_date, end_date) }
     scope :get_by_now, ->(start_date) { where("date >= ? and date <= ?", start_date, Time.now.strftime('%Y%m%d')) }
