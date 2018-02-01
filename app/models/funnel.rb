84943fb413eabd131b1cb89a7105ac2efac4d9b7
billionsoftech <fivewisdomzero@gmail.com>
2018-02-01 14:49:07 -0500

    change table name

--- app/models/funnel.rb
+++ app/models/funnel.rb
@@ -1,5 +1,5 @@
 class Funnel < ApplicationRecord
-    self.table_name = "explorer"
+    self.table_name = "dashboard_overview"
 
     scope :get_by_date_range, ->(start_date, end_date) { where("date >= ? and date <= ?", start_date, end_date) }
     scope :get_by_now, ->(start_date) { where("date >= ? and date <= ?", start_date, Time.now.strftime('%Y%m%d')) }
