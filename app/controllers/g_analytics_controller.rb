a3834bd4b9be1ed106628f09ede48fab21eb46ca
billionsoftech <fivewisdomzero@gmail.com>
2018-02-01 07:58:26 -0500

    change active record execution

--- app/controllers/g_analytics_controller.rb
+++ app/controllers/g_analytics_controller.rb
@@ -8,14 +8,16 @@ class GAnalyticsController < ApplicationController
     def get_by_date_range
         start_date = params[:start_date]
         end_date = params[:end_date]
-        @analytics = GAnalytic.get_by_date_range(start_date, end_date)
+        # @analytics = GAnalytic.get_by_date_range(start_date, end_date)
+        @analytics = ActiveRecord::Base.connection.execute("Select * from explorer WHERE date >= ? and date <= ?", start_date, end_date)
         render json: @analytics.to_json
     end
 
     # GET /g_analytics/:start_date
     def get_by_now
         start_date = params[:start_date]
-        @analytics = GAnalytic.get_by_now(start_date)
+        # @analytics = GAnalytic.get_by_now(start_date)
+        @analytics = ActiveRecord::Base.connection.execute("Select * from explorer WHERE date >= ? and date <= ?", start_date, Time.now.strftime('%Y%m%d'))
         render json: @analytics.to_json
     end
 
@@ -23,7 +25,8 @@ class GAnalyticsController < ApplicationController
     def get_analytics
         start_date = params[:start_date]
         end_date = params[:end_date]
-        @analytics = GAnalytic.get_by_date_range(start_date, end_date).select("sum(sessions)")
+        # @analytics = GAnalytic.get_by_date_range(start_date, end_date).select("sum(sessions)")
+        @analytics = ActiveRecord::Base.connection.execute("Select sum(sessions) from explorer WHERE date >= ? and date <= ?", start_date, end_date)
         render json: @analytics.to_json
     end
 end
